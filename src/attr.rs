a2f4e09cc190b35b80a45da8a2c63e61dc18492c
David Tolnay <dtolnay@gmail.com>
2022-01-07 19:49:42 -0800

    Format doc attributes in comment style

--- src/attr.rs
+++ src/attr.rs
@@ -1,5 +1,6 @@
 use crate::algorithm::Printer;
-use syn::{AttrStyle, Attribute};
+use proc_macro2::TokenTree;
+use syn::{AttrStyle, Attribute, Lit, PathArguments};
 
 impl Printer {
     pub fn outer_attrs(&mut self, attrs: &[Attribute]) {
@@ -19,14 +20,48 @@ impl Printer {
     }
 
     fn attr(&mut self, attr: &Attribute) {
-        self.word(match attr.style {
-            AttrStyle::Outer => "#",
-            AttrStyle::Inner(_) => "#!",
-        });
-        self.word("[");
-        self.path(&attr.path);
-        self.tokens(&attr.tokens);
-        self.word("]");
+        if let Some(doc) = doc(attr) {
+            self.word(match attr.style {
+                AttrStyle::Outer => "///",
+                AttrStyle::Inner => "//!",
+            });
+            self.word(doc);
+        } else {
+            self.word(match attr.style {
+                AttrStyle::Outer => "#",
+                AttrStyle::Inner(_) => "#!",
+            });
+            self.word("[");
+            self.path(&attr.path);
+            self.tokens(&attr.tokens);
+            self.word("]");
+        }
         self.hardbreak();
+    }
+}
+
+fn doc(attr: &Attribute) -> Option<String> {
+    let is_doc = attr.path.leading_colon.is_none()
+        && attr.path.segments.len() == 1
+        && matches!(attr.path.segments[0].arguments, PathArguments::None)
+        && attr.path.segments[0].ident == "doc";
+    if !is_doc {
+        return None;
+    }
+    let mut tokens = attr.tokens.clone().into_iter();
+    match tokens.next() {
+        Some(TokenTree::Punct(punct)) if punct.as_char() == '=' => {}
+        _ => return None,
+    }
+    let literal = match tokens.next() {
+        Some(TokenTree::Literal(literal)) => literal,
+        _ => return None,
+    };
+    if tokens.next().is_some() {
+        return None;
+    }
+    match Lit::new(literal) {
+        Lit::Str(string) => Some(string.value()),
+        _ => None,
     }
 }
