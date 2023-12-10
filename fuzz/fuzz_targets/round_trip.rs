11ebfab3c1a1b4a297d6b9496f28925e90c2999e
David Tolnay <dtolnay@gmail.com>
2023-12-09 18:57:00 -0800

    Format with new rustfmt that handles let-else

--- fuzz/fuzz_targets/round_trip.rs
+++ fuzz/fuzz_targets/round_trip.rs
@@ -5,7 +5,11 @@ use std::str;
 
 fuzz_target!(|data: &[u8]| {
     let ..=299 = data.len() else { return };
-    let Ok(string) = str::from_utf8(data) else { return };
-    let Ok(syntax_tree) = syn::parse_file(string) else { return };
+    let Ok(string) = str::from_utf8(data) else {
+        return;
+    };
+    let Ok(syntax_tree) = syn::parse_file(string) else {
+        return;
+    };
     let _ = prettyplease::unparse(&syntax_tree);
 });
