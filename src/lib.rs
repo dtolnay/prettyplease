179974cc93c8d54894483463c1eea4df9e70a694
David Tolnay <dtolnay@gmail.com>
2024-05-06 21:16:23 -0700

    Release 0.2.20

--- src/lib.rs
+++ src/lib.rs
@@ -320,7 +320,7 @@
 //! these situations with conditional punctuation tokens whose selection can be
 //! deferred and populated after it's known that the group is or is not broken.
 
-#![doc(html_root_url = "https://docs.rs/prettyplease/0.2.19")]
+#![doc(html_root_url = "https://docs.rs/prettyplease/0.2.20")]
 #![allow(
     clippy::cast_possible_wrap,
     clippy::cast_sign_loss,
